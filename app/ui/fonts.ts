0aa7e3f96dd65e1379a18cbc94c014cedbffc4b5
Prince Jules Correa <jrincepules@gmail.com>
2024-02-14 11:39:21 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Cabin, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const cabin = Cabin({ subsets: ['latin'] });
